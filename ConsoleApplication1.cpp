99da6055ebbeeead6ab747cbd12cf98b2e68faa0
DzWang0915 <50360935+DzWang0915@users.noreply.github.com>
2019-05-07 05:36:42 -0400

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include"pch.h"
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "Welcome to Dz's github world." << '\n';
+	cout << "From this day to the end of my days, I'll keep working on developing games.\n";
+	cout << "Hopefully, I can get into the world I truly love" << endl;
+
+
+	cout << "Netease, Valve, Riot, EA, Rockstar, Naughty Dog, 2k. I hope I can join you one day!!!" << endl;
+
+	/*Never stop improving myself, nver say I've tried. Only I'm still trying.*/
+	return 0;
+}
